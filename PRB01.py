74747756aba5df610949c85eaf9928d6892a2d84
ankitpipalia <ankit.pipalia009@gmail.com>
2021-10-16 20:45:15 +0530

    Unsuccessful Submission

--- PRB01.py
+++ PRB01.py
@@ -1,19 +1,20 @@
+def checkPrimality(num):
+    isprime = 0
+    for x in range(2, num-1):
+        if num % x == 0:
+            isprime = 1
+            break
+
+    if isprime:
+        print("yes")
+    else:
+        print("no")
+
+
 tcase = int(input())
 
 while(tcase):
-
     num = int(input())
-    flag = 0
-    
-    for x in range(2,num-1):
-        if(num % x == 0):
-            flag = 1
-    
-    if (flag == 1):
-        print("no")
-    else:
-        print("yes")
-
-
-
+    checkPrimality(num)
     tcase -= 1
+
